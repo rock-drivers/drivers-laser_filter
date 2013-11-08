e9e3f1ca79fc1b647d421d29a5ef95fb48eb28a0
Janosch Machowinski <scotch@imoby-Laptop-1.(none)>
2013-11-08 16:21:34 +0100

    register visualizer

---
+++ viz/vizkit_plugin.rb
@@ -0,0 +1,2 @@
+Vizkit::UiLoader.register_3d_plugin('BoxVisualization', 'laser_filter', 'BoxVisualization')
+Vizkit::UiLoader.register_3d_plugin_for('BoxVisualization', "laser_filter/Box", :updateBox )
